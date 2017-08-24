c2225ff370326afc301b3b909f9491832aa3ef21
Jason Tham <jason.tham@tempus.com>
2017-08-24 14:36:20 -0500

    updated to user routers

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,16 +6,9 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions, renderers, viewsets
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view, detail_route
+from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 # Create your views here.
 class SnippetViewSet(viewsets.ModelViewSet):
