2c807978db728158fecf60a40a51c4edc813a340
Jason Tham <jason.tham@tempus.com>
2017-08-24 12:57:10 -0500

    Added endpoints for User models

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 # Create your views here.
 class SnippetList(generics.ListCreateAPIView):
@@ -13,3 +14,11 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
